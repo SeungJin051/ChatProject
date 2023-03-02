1ddb47175aef6b760bb13d9e88861890cbe4d6b6
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-03-02 16:12:12 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("서버 연결 성공!");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("새로운 메세지 :", message.data, "를 서버로 부터 받아왔습니다.");
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버 연결 해제!");
+});
+
+setTimeout(() => {
+  socket.send("브라우저 : Hello. ");
+}, 10000);
