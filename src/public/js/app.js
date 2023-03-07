5fafc2c5aaef044007645fd49b76f5964fe39afa
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-03-07 16:50:55 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,24 @@
 const socket = io(); // function io
+
+const welcome = document.getElementById("welcome");
+const form = document.querySelector("form");
+
+function backEndDone(msg) {
+  console.log("back-end : ", msg);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  // let nowDate = new Date();
+  // emit + send + send
+  socket.emit(
+    "enter_room",
+    input.value,
+    //nowDate.toDateString(),
+    backEndDone // 종료는 항상 마지막에 !
+  ); // socket.io ( send )
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
