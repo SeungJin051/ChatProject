67b16c6124aa11228099708835a653b22c1c4eed
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2023-03-08 16:38:39 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,11 +8,18 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(message) {
+  const ul = room.querySelector("#chatList");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = document.querySelector(".roomName");
-  h3.innerText = `현재:${roomName}방에 있습니다.`;
+  h3.innerText = `Room = ${roomName}`;
 }
 
 function backEndDone(msg) {
@@ -35,3 +42,7 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("누군가 들어왔습니다!");
+});
